2dac755a22a52315e49ae11e98ec83876cffde46
Vladislav Borovinskiy <imborovinskiy@gmail.com>
2024-12-01 17:51:49 +0300

    fix getDiff...js

--- src/getDifference.js
+++ src/getDifference.js
@@ -4,6 +4,25 @@ import parseFile from './parsers.js';
 
 const getAbsPath = (fileName) => path.resolve(cwd(), '__fixtures__', fileName);
 
+const buildDifference = (key, obj1, obj2, recurse) => {
+  if (obj1[key] instanceof Object && obj2[key] instanceof Object) {
+    return { [`  ${key}`]: recurse(obj1[key], obj2[key]) };
+  }
+  if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
+    return { [`- ${key}`]: obj1[key] };
+  }
+  if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
+    return { [`+ ${key}`]: obj2[key] };
+  }
+  if (obj1[key] !== obj2[key]) {
+    return {
+      [`- ${key}`]: obj1[key],
+      [`+ ${key}`]: obj2[key],
+    };
+  }
+  return { [`  ${key}`]: obj1[key] };
+};
+
 const getDifference = (filepath1, filepath2) => {
   const object1 = parseFile(getAbsPath(filepath1));
   const object2 = parseFile(getAbsPath(filepath2));
@@ -12,19 +31,8 @@ const getDifference = (filepath1, filepath2) => {
     const keys = [...Object.keys({ ...obj1, ...obj2 })].sort();
 
     return keys.reduce((acc, key) => {
-      if (obj1[key] instanceof Object && obj2[key] instanceof Object) {
-        return { ...acc, [`  ${key}`]: inner(obj1[key], obj2[key]) };
-      }
-      if (Object.hasOwn(obj1, key) && !Object.hasOwn(obj2, key)) {
-        return { ...acc, [`- ${key}`]: obj1[key] };
-      }
-      if (!Object.hasOwn(obj1, key) && Object.hasOwn(obj2, key)) {
-        return { ...acc, [`+ ${key}`]: obj2[key] };
-      }
-      if (obj1[key] !== obj2[key]) {
-        return { ...acc, [`- ${key}`]: obj1[key], [`+ ${key}`]: obj2[key] };
-      }
-      return { ...acc, [`  ${key}`]: obj1[key] };
+      const difference = buildDifference(key, obj1, obj2, inner);
+      return { ...acc, ...difference };
     }, {});
   };
 
